be8e1efb43233e2e06a54b77a0dcc8719beb5717
christophszcz <christoph.szczechowicz@gmail.com>
2016-12-31 23:28:17 -0500

    Concatenated the js and css files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,17 @@
-Gruntfile.js
+module.exports = function (grunt){
+	grunt.initConfig({
+
+	  concat: {
+	    js: {
+	      src: ['js/main-section.js'],
+	      dest: 'js/scripts.js',
+	    },
+	    css: {
+	      src: ['css/main.css','css/style.css', ],
+	      dest: 'css/styles.css',
+	    },
+	  },
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+};	
